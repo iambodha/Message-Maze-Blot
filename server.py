9f75c783f5ab7a376b5c997f2486a25710d3231c
iambodha <165776420+iambodha@users.noreply.github.com>
2024-06-30 18:16:28 +0200

    Setup for Hosting

--- server.py
+++ server.py
@@ -2,8 +2,17 @@ from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 import uvicorn
 import messagemaze as mm
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"], 
+)
 
 @app.get("/getMaze")
 async def get_maze(query: str):
